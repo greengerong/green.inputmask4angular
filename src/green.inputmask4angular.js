a9a0e758d74b6832b6be61689f8b70a7374e791e
greengerongg <grzx2210@163.com>
2013-12-12 13:43:45 +0800

    no default for ctp project

--- src/green.inputmask4angular.js
+++ src/green.inputmask4angular.js
@@ -1,58 +1,56 @@
 'use strict';
 
-angular.module('green.inputmask4angular', [])
-    .directive("inputMask", ["$timeout", function ($timeout) {
-        return {
-            require: 'ngModel',
-            link: function (scope, elm, attrs, ngModel) {
-                var applyModelEvents = ["oncomplete", "onKeyUp", "onKeyValidation"],
-                    maskType = "mask";
+angular.module('green.inputmask4angular', []).directive("inputMask", [ "$timeout", function ($timeout) {
+    return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ngModel) {
+            var applyModelEvents = [ "oncomplete", "onKeyUp", "onKeyValidation" ], maskType = "mask";
 
-
-                if (attrs.formatOption) {
-                    var formatOption = scope.$eval(attrs.formatOption);
-                    if (formatOption.parser) {
-                        ngModel.$parsers.push(formatOption.parser);
-                    }
-
-                    if (formatOption.formatter) {
-                        ngModel.$formatters.push(formatOption.formatter);
-                    }
-
-                    if (formatOption.isEmpty) {
-                        ngModel.$isEmpty = formatOption.isEmpty;
-                    }
+            if (attrs.formatOption) {
+                var formatOption = scope.$eval(attrs.formatOption);
+                if (formatOption.parser) {
+                    ngModel.$parsers.push(formatOption.parser);
                 }
 
-                var applyModel = function (fun) {
-                    return function () {
-                        (function (args) {
-                            $timeout(function () {
-                                var viewValue = elm.val();
-                                if (viewValue !== ngModel.$viewValue) {
-                                    ngModel.$setViewValue(viewValue);
-                                }
-                                if (fun) {
-                                    fun.apply(scope, args);
-                                }
-                            });
-                        })(Array.prototype.slice.call(arguments));
-                    };
-                };
-
-                var extendOption = function (option) {
-                    var newOption = angular.extend({}, option);
-                    angular.forEach(applyModelEvents, function (key) {
-                        newOption[key] = applyModel(newOption[key]);
-                    });
-
-                    return newOption;
-                };
-
-                if (attrs.inputMask) {
-                    maskType = scope.$eval(attrs.inputMask) || "mask";
+                if (formatOption.formatter) {
+                    ngModel.$formatters.push(formatOption.formatter);
                 }
 
+                if (formatOption.isEmpty) {
+                    ngModel.$isEmpty = formatOption.isEmpty;
+                }
+            }
+
+            var applyModel = function (fun) {
+                return function () {
+                    (function (args) {
+                        $timeout(function () {
+                            var viewValue = elm.val();
+                            if (viewValue !== ngModel.$viewValue) {
+                                ngModel.$setViewValue(viewValue);
+                            }
+                            if (fun) {
+                                fun.apply(scope, args);
+                            }
+                        });
+                    })(Array.prototype.slice.call(arguments));
+                };
+            };
+
+            var extendOption = function (option) {
+                var newOption = angular.extend({}, option);
+                angular.forEach(applyModelEvents, function (key) {
+                    newOption[key] = applyModel(newOption[key]);
+                });
+
+                return newOption;
+            };
+
+            if (attrs.inputMask) {
+                maskType = scope.$eval(attrs.inputMask);
+            }
+
+            if (maskType) {
                 if (angular.isObject(maskType)) {
                     var maskOption = extendOption(maskType);
                     $timeout(function () {
@@ -65,7 +63,7 @@ angular.module('green.inputmask4angular', [])
                     });
                 }
             }
+
         }
     }
-    ])
-;
+} ]);
